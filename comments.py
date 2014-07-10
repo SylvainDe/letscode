9aef0b04b12937a2109022fc2d67797a37252222
Sylvain <sylvain.desodt+github@gmail.com>
2014-07-10 20:39:40 +0100

    Moving the comment logic out of the languages file. Also adding testsv

---
+++ comments.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8
+"""Functions to comment string."""
+
+from helper import print_warning
+
+def comment_string_with_inline(string, inline_comment):
+    """Return string commented using inline comments"""
+    if not string:
+        return string
+    return '\n'.join(inline_comment + ' ' +
+             s.strip() for s in string.split('\n')) + '\n'
+
+def comment_string_with_block(string, block_comment):
+    """Return string commented using block comments"""
+    if not string:
+        return string
+    beg, end = block_comment
+    return beg + ' ' + string.replace(beg, '').replace(end, '') + ' ' + end
+
+
+def comment_string(string, inline_comment, block_comment):
+    """Return string commented in the best way"""
+    if block_comment is not None and (inline_comment is None or '\n' in string):
+        return comment_string_with_block(string, block_comment)
+    elif inline_comment is not None:
+        assert block_comment is None or '\n' not in string
+        return comment_string_with_inline(string, inline_comment)
+    else:
+        print_warning('Cannot comment string')
+        return ''  # Empty string is safe and shoudn't cause troubles
+
+
+# vim: filetype=python tabstop=8 expandtab shiftwidth=4 softtabstop=4
---
+++ comments_tests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8
+"""Unit tests for code in comments.py."""
+
+from comments import comment_string_with_inline, \
+        comment_string_with_block, \
+        comment_string
+import unittest
+
+class TestCommentString(unittest.TestCase):
+    """Unit tests related to string commenting"""
+    def test_inline_comment(self):
+        """Unit tests related to inline comments"""
+        self.assertEqual(comment_string_with_inline('', None), '')
+        self.assertEqual(comment_string_with_inline('', '#'), '')
+        self.assertEqual(comment_string_with_inline('a', '#'), '# a\n')
+        self.assertEqual(comment_string_with_inline('a\nb', '#'), '# a\n# b\n')
+
+    def test_block_comment(self):
+        """Unit tests related to block comments"""
+        self.assertEqual(comment_string_with_block('', None), '')
+        self.assertEqual(comment_string_with_block('', ('/*', '*/')), '')
+        self.assertEqual(comment_string_with_block('a', ('/*', '*/')), '/* a */')
+        self.assertEqual(comment_string_with_block('a\nb', ('/*', '*/')), '/* a\nb */')
+
+    def test_comment(self):
+        """Unit tests related to inline comments"""
+        self.assertEqual(comment_string('', None, None), '')
+        self.assertEqual(comment_string('a', None, None), '')
+        self.assertEqual(comment_string('a', '#', None), '# a\n')
+        self.assertEqual(comment_string('a', None, ('/*', '*/')), '/* a */')
+        self.assertEqual(comment_string('a', '#', ('/*', '*/')), '# a\n')
+        self.assertEqual(comment_string('a\nb', '#', None), '# a\n# b\n')
+        self.assertEqual(comment_string('a\nb', '#', ('/*', '*/')), '/* a\nb */')
+
+# vim: filetype=python tabstop=8 expandtab shiftwidth=4 softtabstop=4
